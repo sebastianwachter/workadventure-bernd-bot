6cc2fbb31b88940e1f521a7eeed51324d8b5914d
sebastianwachter <sebastian.wachter@online.de>
2026-09-25 08:59:12 +0000

    Deploying to gh-pages from master @ cb1a092c7978b7589007148ed3f4edf619ecc3b6 🚀

--- bot.js
+++ bot.js
@@ -1,22 +1,22 @@
-const e = { scope: "bubble" }, a = {
+const e = { scope: "bubble" }, s = {
   x: 0,
   y: 0
-};
-function s() {
+}, o = "https://1020993654.rsc.cdn77.org/Stromberg/867/1080-HLS/867_1080-HLS_.m3u8";
+function r() {
   WA.chat.sendChatMessage("Der Papa ist hier!", e);
 }
-const i = {
-  run: async (o) => {
-    await WA.onInit(), WA.chat.open(), s(), WA.chat.onChatMessage((n) => {
+const c = {
+  run: async (i) => {
+    await WA.onInit(), WA.chat.open(), r(), WA.chat.onChatMessage((n) => {
       n === "Ping" && WA.chat.sendChatMessage("Pong!", e);
     }, { scope: "bubble" });
-    const t = await WA.player.getPosition();
-    WA.room.website.create({
+    const t = await WA.player.getPosition(), a = new URL("stream.html", import.meta.url);
+    a.searchParams.set("src", o), WA.room.website.create({
       name: "Bernd",
-      url: "https://1020993654.rsc.cdn77.org/Stromberg/867/1080-HLS/867_1080-HLS_.m3u8",
+      url: a.toString(),
       position: {
-        x: t.x + a.x,
-        y: t.y + a.y,
+        x: t.x + s.x,
+        y: t.y + s.y,
         width: 1280,
         height: 720
       },
@@ -29,5 +29,5 @@ const i = {
   }
 };
 export {
-  i as default
+  c as default
 };
