2ad2dde8460dca26646b3946f78a8225abda1d15
sebastianwachter <sebastian.wachter@online.de>
2026-09-25 10:09:48 +0000

    Deploying to gh-pages from master @ 7f9dc992f92f652aceb727c9d26b785e384b6526 🚀

--- bot.js
+++ bot.js
@@ -1,17 +1,21 @@
-const e = { scope: "bubble" }, s = "https://1020993654.rsc.cdn77.org/Stromberg/867/1080-HLS/867_1080-HLS_.m3u8", n = "https://sebastianwachter.github.io/workadventure-bernd-bot/";
+const t = { scope: "bubble" }, s = "https://1020993654.rsc.cdn77.org/Stromberg/867/1080-HLS/867_1080-HLS_.m3u8", n = "https://sebastianwachter.github.io/workadventure-bernd-bot/";
 function r() {
-  WA.chat.sendChatMessage("Der Papa ist hier!", e);
+  WA.chat.sendChatMessage("Der Papa ist hier!", t);
 }
 const i = {
   run: async (o) => {
-    await WA.onInit(), WA.chat.open(), r(), WA.chat.onChatMessage((t) => {
-      t === "Ping" && WA.chat.sendChatMessage("Pong!", e);
-    }, { scope: "bubble" });
+    await WA.onInit(), WA.chat.open(), r(), WA.chat.onChatMessage((e) => {
+      e === "Ping" && WA.chat.sendChatMessage("Pong!", t);
+    }, { scope: "bubble" }), await WA.players.configureTracking({ players: !0, movement: !0 }), WA.players.onPlayerEnters.subscribe((e) => {
+      WA.chat.sendChatMessage(`${e.name} taucht auf bei x=${e.position.x}, y=${e.position.y}`, t);
+    }), WA.players.onPlayerMoves.subscribe(({ player: e, newPosition: a }) => {
+      WA.chat.sendChatMessage(`${e.name} steht jetzt bei x=${a.x}, y=${a.y}`, t);
+    });
     try {
-      const t = new URL("stream.html", n);
-      t.searchParams.set("src", s), WA.room.website.create({
+      const e = new URL("stream.html", n);
+      e.searchParams.set("src", s), WA.room.website.create({
         name: "Bernd",
-        url: t.toString(),
+        url: e.toString(),
         position: {
           x: 0,
           y: 0,
@@ -25,9 +29,9 @@ const i = {
         scale: 1
       });
       const a = await WA.player.getPosition();
-      WA.chat.sendChatMessage(`Stream läuft bei x=${a.x}, y=${a.y}`, e);
-    } catch (t) {
-      WA.chat.sendChatMessage(`Stream ging nicht auf: ${t}`, e);
+      WA.chat.sendChatMessage(`Stream läuft bei x=${a.x}, y=${a.y}`, t);
+    } catch (e) {
+      WA.chat.sendChatMessage(`Stream ging nicht auf: ${e}`, t);
     }
   }
 };
