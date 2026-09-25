cac7f537d813e5f32cd453c3d4c15bc2d7e880f9
sebastianwachter <sebastian.wachter@online.de>
2026-09-25 09:48:27 +0000

    Deploying to gh-pages from master @ 5f7e9b496e69430fe9e642dcd087a122a3c957c8 🚀

--- bot.js
+++ bot.js
@@ -1,47 +1,36 @@
-const a = { scope: "bubble" }, n = {
-  x: 0,
-  y: 0
-}, r = "https://1020993654.rsc.cdn77.org/Stromberg/867/1080-HLS/867_1080-HLS_.m3u8", i = "https://sebastianwachter.github.io/workadventure-bernd-bot/";
-function o() {
-  WA.chat.sendChatMessage("Der Papa ist hier!", a);
+const e = { scope: "bubble" }, s = "https://1020993654.rsc.cdn77.org/Stromberg/867/1080-HLS/867_1080-HLS_.m3u8", n = "https://sebastianwachter.github.io/workadventure-bernd-bot/";
+function r() {
+  WA.chat.sendChatMessage("Der Papa ist hier!", e);
 }
-const h = {
-  run: async (c) => {
-    await WA.onInit(), WA.chat.open(), o(), WA.chat.onChatMessage((t) => {
-      t === "Ping" && WA.chat.sendChatMessage("Pong!", a);
+const i = {
+  run: async (o) => {
+    await WA.onInit(), WA.chat.open(), r(), WA.chat.onChatMessage((t) => {
+      t === "Ping" && WA.chat.sendChatMessage("Pong!", e);
     }, { scope: "bubble" });
-    const s = await WA.player.getPosition();
     try {
-      const t = new URL("stream.html", i);
-      t.searchParams.set("src", r), WA.room.website.create({
+      const t = new URL("stream.html", n);
+      t.searchParams.set("src", s), WA.room.website.create({
         name: "Bernd",
         url: t.toString(),
         position: {
-          x: s.x + n.x,
-          y: s.y + n.y,
+          x: 0,
+          y: 0,
           width: 1280,
           height: 720
         },
         visible: !0,
         allowApi: !0,
         allow: "autoplay; encrypted-media; fullscreen",
-        origin: "map",
+        origin: "player",
         scale: 1
-      }), WA.chat.sendChatMessage("Ich zeig jetzt den Stream", a);
-      try {
-        const e = await WA.room.website.get("Bernd");
-        WA.chat.sendChatMessage(
-          `Debug: WA kennt "Bernd" bei x=${e.x} y=${e.y} w=${e.width} h=${e.height} visible=${e.visible} url=${e.url}`,
-          a
-        );
-      } catch (e) {
-        WA.chat.sendChatMessage(`Debug: WA.room.website.get("Bernd") ist explodiert: ${e}`, a);
-      }
+      });
+      const a = await WA.player.getPosition();
+      WA.chat.sendChatMessage(`Stream läuft bei x=${a.x}, y=${a.y}`, e);
     } catch (t) {
-      WA.chat.sendChatMessage(`Stream ging nicht auf: ${t}`, a);
+      WA.chat.sendChatMessage(`Stream ging nicht auf: ${t}`, e);
     }
   }
 };
 export {
-  h as default
+  i as default
 };
