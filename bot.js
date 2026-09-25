dc40fe15609761c16c4719e415f6a297d88de79b
sebastianwachter <sebastian.wachter@online.de>
2026-09-25 07:45:14 +0000

    Deploying to gh-pages from master @ 1811e69232faae23cd4800787f40eb408a1cacb4 🚀

--- bot.js
+++ bot.js
@@ -1,29 +1,48 @@
-const c = "2yJgwwDcgV8", h = "bernd-test-stream", n = "therapy-room", r = `https://www.youtube.com/embed/${c}?autoplay=1&mute=1&loop=1&playlist=${c}&controls=1`, o = { scope: "bubble" }, l = {
-  run: async (s) => {
-    await WA.onInit();
-    const i = await WA.room.area.get(n).catch(() => {
+class a {
+  init() {
+    WA.chat.onChatMessage((e, n) => {
+      console.log(`Received chat message: ${e}`, n), WA.chat.sendChatMessage(`Ich habe deine Nachricht erhalten: "${e}"`, { scope: "bubble" }), e.includes("Ping") && WA.chat.sendChatMessage("Pong!", { scope: "bubble" });
     });
-    if (!i)
-      WA.chat.sendChatMessage(`Hab den "${n}" nicht auf der Karte gefunden, ich bleib stehen wo ich bin.`, o);
-    else {
-      const e = await WA.player.getPosition();
-      e.x >= i.x && e.x <= i.x + i.width && e.y >= i.y && e.y <= i.y + i.height || (WA.chat.sendChatMessage(`Bin nicht im "${n}", ich lauf da mal hin.`, o), await WA.player.moveTo(i.x + i.width / 2, i.y + i.height / 2));
-    }
-    WA.room.website.create({
-      name: h,
-      url: r,
-      position: {
-        x: (s == null ? void 0 : s.streamX) ?? (i == null ? void 0 : i.x) ?? 0,
-        y: (s == null ? void 0 : s.streamY) ?? (i == null ? void 0 : i.y) ?? 0,
-        width: (s == null ? void 0 : s.streamWidth) ?? 1280,
-        height: (s == null ? void 0 : s.streamHeight) ?? 720
-      },
-      visible: !0,
-      allow: "autoplay; encrypted-media; fullscreen",
-      origin: "map"
-    }), WA.chat.sendChatMessage(`Ich zeig jetzt "${h}" für immer`, o);
   }
+}
+const t = new a();
+WA.onInit().then(async () => {
+  t.init();
+});
+const s = {
+  // run: async (metadata: any) => {
+  //     await WA.onInit();
+  //     const therapyRoom = await WA.room.area.get(THERAPY_ROOM_AREA).catch(() => undefined);
+  //     if (!therapyRoom) {
+  //         WA.chat.sendChatMessage(`Hab den "${THERAPY_ROOM_AREA}" nicht auf der Karte gefunden, ich bleib stehen wo ich bin.`, CHAT_OPTIONS);
+  //     } else {
+  //         const position = await WA.player.getPosition();
+  //         const isInside =
+  //             position.x >= therapyRoom.x &&
+  //             position.x <= therapyRoom.x + therapyRoom.width &&
+  //             position.y >= therapyRoom.y &&
+  //             position.y <= therapyRoom.y + therapyRoom.height;
+  //         if (!isInside) {
+  //             WA.chat.sendChatMessage(`Bin nicht im "${THERAPY_ROOM_AREA}", ich lauf da mal hin.`, CHAT_OPTIONS);
+  //             await WA.player.moveTo(therapyRoom.x + therapyRoom.width / 2, therapyRoom.y + therapyRoom.height / 2);
+  //         }
+  //     }
+  //     WA.room.website.create({
+  //         name: STREAM_NAME,
+  //         url: EMBED_URL,
+  //         position: {
+  //             x: metadata?.streamX ?? therapyRoom?.x ?? 0,
+  //             y: metadata?.streamY ?? therapyRoom?.y ?? 0,
+  //             width: metadata?.streamWidth ?? 1280,
+  //             height: metadata?.streamHeight ?? 720,
+  //         },
+  //         visible: true,
+  //         allow: "autoplay; encrypted-media; fullscreen",
+  //         origin: "map",
+  //     });
+  //     WA.chat.sendChatMessage(`Ich zeig jetzt "${STREAM_NAME}" für immer`, CHAT_OPTIONS);
+  // }
 };
 export {
-  l as default
+  s as default
 };
