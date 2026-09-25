4d7c41fcd6df418279202d2e4248e24f0a1eda55
sebastianwachter <sebastian.wachter@online.de>
2026-09-25 09:09:44 +0000

    Deploying to gh-pages from master @ e809f7bb9720e5cd8c88ca73de0b805def999121 🚀

--- bot.js
+++ bot.js
@@ -1,33 +1,38 @@
 const e = { scope: "bubble" }, s = {
   x: 0,
   y: 0
-}, o = "https://1020993654.rsc.cdn77.org/Stromberg/867/1080-HLS/867_1080-HLS_.m3u8";
+}, n = "https://1020993654.rsc.cdn77.org/Stromberg/867/1080-HLS/867_1080-HLS_.m3u8", o = "https://sebastianwachter.github.io/workadventure-bernd-bot/";
 function r() {
   WA.chat.sendChatMessage("Der Papa ist hier!", e);
 }
-const c = {
-  run: async (i) => {
-    await WA.onInit(), WA.chat.open(), r(), WA.chat.onChatMessage((n) => {
-      n === "Ping" && WA.chat.sendChatMessage("Pong!", e);
+const i = {
+  run: async (c) => {
+    await WA.onInit(), WA.chat.open(), r(), WA.chat.onChatMessage((t) => {
+      t === "Ping" && WA.chat.sendChatMessage("Pong!", e);
     }, { scope: "bubble" });
-    const t = await WA.player.getPosition(), a = new URL("stream.html", import.meta.url);
-    a.searchParams.set("src", o), WA.room.website.create({
-      name: "Bernd",
-      url: a.toString(),
-      position: {
-        x: t.x + s.x,
-        y: t.y + s.y,
-        width: 1280,
-        height: 720
-      },
-      visible: !0,
-      allowApi: !0,
-      allow: "autoplay; encrypted-media; fullscreen",
-      origin: "map",
-      scale: 1
-    }), WA.chat.sendChatMessage("Ich zeig jetzt den Stream", e);
+    const a = await WA.player.getPosition();
+    try {
+      const t = new URL("stream.html", o);
+      t.searchParams.set("src", n), WA.room.website.create({
+        name: "Bernd",
+        url: t.toString(),
+        position: {
+          x: a.x + s.x,
+          y: a.y + s.y,
+          width: 1280,
+          height: 720
+        },
+        visible: !0,
+        allowApi: !0,
+        allow: "autoplay; encrypted-media; fullscreen",
+        origin: "map",
+        scale: 1
+      }), WA.chat.sendChatMessage("Ich zeig jetzt den Stream", e);
+    } catch (t) {
+      WA.chat.sendChatMessage(`Stream ging nicht auf: ${t}`, e);
+    }
   }
 };
 export {
-  c as default
+  i as default
 };
