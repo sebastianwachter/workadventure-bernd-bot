77797cdcc2925c2a0c2fa8c20a3feda1a73fde34
sebastianwachter <sebastian.wachter@online.de>
2026-09-25 08:15:06 +0000

    Deploying to gh-pages from master @ a7deeb100afd3361dcb2638374fe4c88e59e1668 🚀

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const s = { scope: "bubble" };
 console.log("Script started successfully");
 const c = {
   run: async (a) => {
-    console.log("Script started successfully 2 ", a), await WA.onInit(), WA.chat.open(), WA.chat.sendChatMessage("PiPing", s), WA.chat.onChatMessage((e, t) => {
+    console.log("Script started successfully 2 ", a), await WA.onInit(), WA.chat.open(), WA.chat.sendChatMessage("PiPiPiPing", s), WA.chat.onChatMessage((e, t) => {
       console.log(`Received chat message: ${e}`, t), WA.chat.sendChatMessage(`Ich habe deine Nachricht erhalten: "${e}"`, s), e === "Ping" && WA.chat.sendChatMessage("Pong!", s);
     });
   }
